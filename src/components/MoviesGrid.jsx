4d4992862b2641aefae83d2f55596aa51b1bbf21
Alejandra Primo D <heidyprimo10@outlook.com>
2021-06-30 18:14:09 -0500

    Agregando paquete de archivos completo de MOVIETIME

---
+++ src/components/MoviesGrid.jsx
@@ -0,0 +1,43 @@
+import { MovieCard } from "./MovieCard";
+import styles from "./MoviesGrid.module.css";
+import { useEffect, useState } from "react";
+import { get } from "../utils/httpClient";
+import {Spinner} from './Spinner';
+import { useQuery } from "../hooks/useQuery";
+
+
+export function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const query =useQuery();
+  const search = query.get("search");
+  console.log(search);
+
+ {/*const location = useLocation();
+ console.log(location.search);*/} 
+ //query paramams= parametros de consulta(react-router-dom)
+
+  useEffect(()=>{
+    setIsLoading(true);
+    const searchUrl = search 
+    ? "/search/movie?query=" + search 
+    : "/discover/movie";
+    get(searchUrl).then((data) => {
+       setMovies(data.results); 
+       setIsLoading(false);
+      })
+  },[search]);
+
+  if(isLoading){
+    return <Spinner/>;
+  }
+
+  return (
+    <ul className={styles.moviesGrid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,35 @@
+import  styles from './Search.module.css';
+import { FaSearch } from 'react-icons/fa'
+import { useEffect, useState } from 'react';
+import { useHistory} from 'react-router';
+import { useQuery } from '../hooks/useQuery';
+
+export function Search() {
+    
+    const query= useQuery();
+    const search = query.get("search");
+
+    const [searchText, setSearchText] = useState(" ");
+    const history = useHistory(); //se usa para cambio de ruta o historial de la barra de navegacion 
+
+    useEffect(() =>{
+        setSearchText(search || ""); 
+    }, [search]);
+
+
+    const handleSubmit=(e) => {
+        e.preventDefault();
+        history.push("/?search=" + searchText);
+
+
+    }
+    return (
+        <form className={styles.searchContainer} onSubmit={handleSubmit}>
+            <div className={styles.searchBox}>
+                <input className={styles.searchInput} type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+                <button className={styles.searchButton} type="submit" ><FaSearch size={20} />
+                </button>
+            </div>
+        </form>
+    )
+}
